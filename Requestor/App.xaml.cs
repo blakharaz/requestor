c4dded5f8a8c9839480ce86347b1fe44848bb3ea
Blakharaz <blakharaz@anfractus.de>
2024-06-14 23:07:53 +0200

    Initial project with default Blazor Hybrid app

---
+++ Requestor/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Requestor;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
+
---
+++ Requestor/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Requestor;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
